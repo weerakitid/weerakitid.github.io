1d266f131def4aecdb5e5814b91772eab658eebd
weerakit <weerakitid@gmail.com>
2020-01-29 13:56:48 +0700

    add btn:hover

--- src/components/Button.js
+++ src/components/Button.js
@@ -8,6 +8,12 @@ const Button = styled.button`
   background: #f5678d;
   padding: 16px 40px;
   margin-top: 40px;
+  
+  &:disabled {
+    color: #bbbbbb;
+    background: #e0e0e0;
+    cursor: not-allowed;
+  }
 `
 
 export default Button
--- src/components/Details.js
+++ src/components/Details.js
@@ -14,7 +14,7 @@ const Details = () => {
       <ImageHolder src={member} hero top="40px" />
       <Text bold top="40px">Get ticket</Text>
       <Text title top="10px">Sunday 26 January 2020 at 03.00 PM.</Text>
-      <Button>Get ticket now</Button>
+      <Button disabled={true}>Get ticket now</Button>
       <Text footer top="100px">Blackpink in your area</Text>
     </StyledDiv>
   )
